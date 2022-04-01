ad0b5d5dde0bd5d591e2c04175be112dbd3642eb
pedrovitordev <pedro.paula@viannasempre.com.br>
2022-04-01 17:03:48 -0300

    automatizado cenario de cadastro fixo,faker e examples

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -4,14 +4,27 @@ Dado('que acesso o cadastro de usuário') do
 end
   
 Dado('preencho os campos do formulário com dados válidos de pessoa física') do
-    pending # Write code here that turns the phrase above into concrete actions
+    cadastro_page.preencher_form_com_dados_validos_fixos_pf
 end
   
 Quando('confirmo o cadastro') do
-    pending # Write code here that turns the phrase above into concrete actions
+    @cadastro_page.clicar_finalizar
 end
   
 Então('o sistema deve exibir a mensagem de boas vindas') do
-    pending # Write code here that turns the phrase above into concrete actions
+    @cadastro_page.wait_until_msg_sucesso_visible
+    expect(@cadastro_page).to have_content 'Cadastro efetuado com sucesso. Obrigado!'
+end
+
+Dado('preencho os campos do formulário com dados válidos de um novo usuário pessoa física') do
+    @cadastro_page.preencher_form_com_dados_aleatorios_pf
+end
+
+Dado("eu preencha os campos com {string},{string},{string},{string},{string},{string}") do |nome, sobrenome, email, celular, tipo_de_conta, documento|
+    @cadastro_page.preencher_form_com_dados_informados(nome, sobrenome, email, celular, tipo_de_conta, documento)
 end
   
+Então("o sistema deve exibir a mensagem de confirmação {string}") do |msgsucesso|
+    expect(@cadastro_page).to have_content msgsucesso
+end
+   
