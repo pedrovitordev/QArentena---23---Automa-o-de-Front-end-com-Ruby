734ba2e5dca476969ac2370b730802be38399112
pedrovitordev <pedro.paula@viannasempre.com.br>
2022-04-01 17:27:34 -0300

    cenario de cadastro invalido yml

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -39,4 +39,14 @@ end
 E("permanecer na página de cadastro") do
     expect(@cadastro_page).to have_current_path '/qarentena/cadastro/'
 end
-  
+
+Dado("eu informe os campos {string},{string},{string},{string},{string},{string} conforme arquivo") do |nome, sobrenome, email, telefone, tipo_de_conta, documento|
+    @cadastro_page.preencher_form_com_dados_informados(
+      UserData.get(nome),
+      UserData.get(sobrenome),
+      UserData.get(email),
+      UserData.get(telefone),
+      UserData.get(tipo_de_conta),
+      UserData.get(documento)
+    )
+end
